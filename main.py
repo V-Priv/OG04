4e8a7cea8e163f28a83e7f78aadbd0d6eb30d2ba
V-Priv <vpriv10@gmail.com>
2024-08-20 16:09:23 +0300

    Update new-test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(8):
+# вносим изменения в ветке new-test
+for i in range(1):
     print("слово")
 a, b = 9, 8
 if a > b:
@@ -7,4 +8,9 @@ elif b > a:
     print("b больше чем a")
 else:
     print("a равно b")
+
+name = input("Введите имя")
+family_name = input("Введите фамилию")
+print(f'{name} {family_name}')
+
 
